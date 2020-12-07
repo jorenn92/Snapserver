a5dc3438fa88ceb62cadcae614bbdbd8d54d5418
Joren V <jorenn92@gmail.com>
2020-12-07 15:20:17 +0100

    Update spotify_takeover.sh
    use local address

--- scripts/spotify_takeover.sh
+++ scripts/spotify_takeover.sh
@@ -4,4 +4,4 @@ curl -X POST -H Content-Type:application/json -d '{
   "method": "core.playback.stop",
   "jsonrpc": "2.0",
   "id": 1
-}' http://192.168.0.2:6680/mopidy/rpc
+}' http://127.0.0.1:6680/mopidy/rpc
